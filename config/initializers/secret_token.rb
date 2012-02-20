569d0a57ab07324b642e35c2fb8fcd637fdc4b5f
estill01 <ehs.stillman@gmail.com>
2012-02-19 23:56:46 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Duubot::Application.config.secret_token = '61cc883ca805d5b61bfb2815ed0181b040f44652c514b50ac9e571a2a876e8822fadca4ee668b1eba5b2fc1c90d277b71ad65997425ed2126ae0446089b6667e'
